c54d85a4f883ec0a4b0b3dd641381139653cd02d
FP <fp@nomail.com>
2021-08-10 21:09:14 -0700

    fix(tests): joint migration

--- tests/test_joint_migration.py
+++ tests/test_joint_migration.py
@@ -3,48 +3,45 @@ import pytest
 from brownie import Contract, Wei
 
 
-def test_join_migration(chain, accounts, Joint):
-
-    providerB = Contract("0xF878E59600124ca46a30193A3F76EDAc99591698")
-    old_joint = Contract(providerB.joint())
-    providerA = Contract(old_joint.providerA())
-
-    gov = accounts.at(old_joint.governance(), force=True)
-    old_joint.harvest({"from": gov})
+def test_joint_migration(
+    chain, gov, strategist, weth, joint, providerA, providerB, SushiJoint
+):
+    old_joint = joint
+    providerA.harvest({"from": gov})
+    providerB.harvest({"from": gov})
     old_joint.liquidatePosition({"from": gov})
     old_joint.setReinvest(False, {"from": gov})
-    old_joint.harvest({"from": gov})
+    providerA.setInvestWant(False, {"from": strategist})
+    providerB.setInvestWant(False, {"from": strategist})
+    providerA.harvest({"from": gov})
+    providerB.harvest({"from": gov})
 
     assert providerA.balanceOfWant() > 0
     assert providerB.balanceOfWant() > 0
     assert old_joint.balanceOfB() + old_joint.balanceOfA() == 0
     assert old_joint.balanceOfStake() == 0
 
-    new_joint = Joint.deploy(
-        old_joint.governance(),
-        old_joint.strategist(),
-        old_joint.keeper(),
-        old_joint.tokenA(),
-        old_joint.tokenB(),
-        old_joint.router(),
+    new_joint = SushiJoint.deploy(
+        providerA,
+        providerB,
+        joint.router(),
+        weth,
+        joint.masterchef(),
+        joint.reward(),
+        joint.pid(),
         {"from": gov},
     )
 
     providerA.setJoint(new_joint, {"from": gov})
     providerB.setJoint(new_joint, {"from": gov})
-    new_joint.setProviderA(providerA, {"from": gov})
-    new_joint.setProviderB(providerB, {"from": gov})
+
+    providerA.setInvestWant(True, {"from": strategist})
+    providerB.setInvestWant(True, {"from": strategist})
 
     assert providerA.takeProfit() == False
     assert providerB.takeProfit() == False
 
-    vaultA = Contract(providerA.vault())
-    vaultA.updateStrategyMaxDebtPerHarvest(providerA, 0, {"from": gov})
-    vaultB = Contract(providerB.vault())
-    vaultB.updateStrategyMaxDebtPerHarvest(providerB, 0, {"from": gov})
-
     providerA.harvest({"from": gov})
     providerB.harvest({"from": gov})
 
-    # Invest capital
-    new_joint.harvest({"from": gov})
+    assert new_joint.balanceOfStake() > 0
