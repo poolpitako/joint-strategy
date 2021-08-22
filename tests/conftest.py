6980dbd3efc2c4188d97c83da2a157d20d546f40
FP <fp@nomail.com>
2021-08-22 14:26:20 -0700

    fix: test

--- tests/conftest.py
+++ tests/conftest.py
@@ -134,7 +134,7 @@ def providerA(gov, strategist, keeper, vaultA, ProviderStrategy):
     strategy = strategist.deploy(ProviderStrategy, vaultA)
     strategy.setKeeper(keeper)
 
-    vaultA.addStrategy(strategy, 1900, 0, 2 ** 256 - 1, 1_000, {"from": gov})
+    vaultA.addStrategy(strategy, 900, 0, 2 ** 256 - 1, 1_000, {"from": gov})
 
     yield strategy
 
