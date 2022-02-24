3f86afa302ace88fe58e610574e6756128cf0c6c
FP <fp_crypt@protonmail.com>
2022-02-24 10:05:11 -0800

    feat: test imbalance

--- tests/test_harvests.py
+++ tests/test_harvests.py
@@ -187,3 +187,108 @@ def test_manual_exit(
     assert vaultB.strategies(providerB).dict()["totalGain"] > 0
     assert vaultB.strategies(providerB).dict()["totalLoss"] == 0
     assert vaultB.strategies(providerB).dict()["totalDebt"] == 0
+
+
+
+# tests harvesting a strategy that returns profits correctly
+def test_profitable_with_big_imbalance_harvest(
+    chain,
+    accounts,
+    tokenA,
+    tokenB,
+    vaultA,
+    vaultB,
+    providerA,
+    providerB,
+    joint,
+    user,
+    strategist,
+    amountA,
+    amountB,
+    RELATIVE_APPROX,
+    gov,
+    tokenA_whale,
+    tokenB_whale,
+    mock_chainlink,
+    solid_token,
+    sex_token,
+    solid_router,
+    lp_token,
+    lp_depositor_solidex
+):
+    
+    # Deposit to the vault
+    actions.user_deposit(user, vaultA, tokenA, amountA)
+    actions.user_deposit(user, vaultB, tokenB, amountB)
+
+    # Harvest 1: Send funds through the strategy
+    chain.sleep(1)
+
+    actions.gov_start_epoch(
+        gov, providerA, providerB, joint, vaultA, vaultB, amountA, amountB
+    )
+
+    total_assets_tokenA = providerA.estimatedTotalAssets()
+    total_assets_tokenB = providerB.estimatedTotalAssets()
+
+    assert pytest.approx(total_assets_tokenA, rel=1e-2) == amountA
+    assert pytest.approx(total_assets_tokenB, rel=1e-2) == amountB
+
+    profit_amount_percentage = 0.0095
+    profit_amount_tokenA, profit_amount_tokenB = actions.generate_profit(
+        profit_amount_percentage,
+        joint,
+        providerA,
+        providerB,
+        tokenA_whale,
+        tokenB_whale,
+    )
+    
+    before_pps_tokenA = vaultA.pricePerShare()
+    before_pps_tokenB = vaultB.pricePerShare()
+    # Harvest 2: Realize profit
+    chain.sleep(1)
+
+    tokenA.approve(solid_router, 2**256-1, {'from': tokenA_whale})
+    solid_router.swapExactTokensForTokensSimple(
+        10_000_000e6,
+        0,
+        tokenA,
+        tokenB,
+        True,
+        tokenA_whale,
+        2**256-1,
+        {'from': tokenA_whale},
+    )
+
+    actions.gov_end_epoch(gov, providerA, providerB, joint, vaultA, vaultB)
+
+    solid_pre = solid_token.balanceOf(joint)
+    sex_pre = sex_token.balanceOf(joint)
+    assert sex_pre > 0
+    assert solid_pre > 0
+
+    gov_solid_pre = solid_token.balanceOf(gov)
+    gov_sex_pre = sex_token.balanceOf(gov)
+    joint.sweep(solid_token,{"from":gov})
+
+    joint.sweep(sex_token,{"from":gov})
+    
+    assert (solid_token.balanceOf(gov) - gov_solid_pre) == solid_pre
+    assert (sex_token.balanceOf(gov) - gov_sex_pre) == sex_pre
+
+    utils.sleep()  # sleep for 6 hours
+
+    # all the balance (principal + profit) is in vault
+    total_balance_tokenA = vaultA.totalAssets()
+    total_balance_tokenB = vaultB.totalAssets()
+    assert (
+        pytest.approx(total_balance_tokenA, rel=5 * 1e-3)
+        == amountA + profit_amount_tokenA
+    )
+    assert (
+        pytest.approx(total_balance_tokenB, rel=5 * 1e-3)
+        == amountB + profit_amount_tokenB
+    )
+    assert vaultA.pricePerShare() > before_pps_tokenA
+    assert vaultB.pricePerShare() > before_pps_tokenB
