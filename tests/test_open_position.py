77f59c452fc86fde6d2ad1bc35ba1cb23e1764ef
16slim <16slimchance16@gmail.com>
2022-03-10 12:03:47 +0100

    fix: tests passing after nohedge merge

--- tests/test_open_position.py
+++ tests/test_open_position.py
@@ -390,11 +390,7 @@ def test_open_position_price_change_tokenA_rewards(
     assert current_amount_A > initial_amount_A
     assert current_amount_B < initial_amount_B
 
-<<<<<<< HEAD
-    actions.dump_rewards(rewards_whale, 1000e18, router, rewards, joint, tokenB)
-=======
-    actions.airdrop_rewards(rewards_whale, 400e18, router, rewards, joint, tokenB)
->>>>>>> b2a3bde25e35d6d3beaaf58a983a75814fd7361f
+    actions.airdrop_rewards(rewards_whale, 1000e18, router, rewards, joint, tokenB)
     assert joint.balanceOfReward() > 0
 
     utils.print_joint_status(joint, tokenA, tokenB, lp_token, rewards)
@@ -487,11 +483,7 @@ def test_open_position_price_change_tokenB_rewards(
     assert current_amount_A < initial_amount_A
     assert current_amount_B > initial_amount_B
 
-<<<<<<< HEAD
-    actions.dump_rewards(rewards_whale, 1000e18, router, rewards, joint, tokenB)
-=======
-    actions.airdrop_rewards(rewards_whale, 400e18, router, rewards, joint, tokenB)
->>>>>>> b2a3bde25e35d6d3beaaf58a983a75814fd7361f
+    actions.airdrop_rewards(rewards_whale, 1000e18, router, rewards, joint, tokenB)
     assert joint.balanceOfReward() > 0
 
     utils.print_joint_status(joint, tokenA, tokenB, lp_token, rewards)
